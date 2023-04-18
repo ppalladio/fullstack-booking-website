3addacc55ec9ddb351e51689ea37f02e21edaa38
ppalladio <nine.slide@gmail.com>
2023-04-18 20:42:54 +0200

    environment setup

---
+++ airbnb_clone/app/layout.tsx
@@ -0,0 +1,24 @@
+import {Montserrat} from "next/font/google"
+
+import './globals.css'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+const font = Montserrat(
+	{
+		subsets:["latin"]
+	} 
+)
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}

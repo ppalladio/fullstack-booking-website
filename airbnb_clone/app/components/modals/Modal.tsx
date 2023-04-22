422791e003dc8a2f455768eb85c5af70c070043b
ppalladio <nine.slide@gmail.com>
2023-04-22 15:25:22 +0200

    button component finished

--- airbnb_clone/app/components/modals/Modal.tsx
+++ airbnb_clone/app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useCallback, useEffect, useState } from 'react';
 import {IoMdClose} from 'react-icons/io'
+import Button from '../Button';
 interface ModalProps {
     isOpen?: boolean;
     onClose: () => void;
@@ -79,6 +80,20 @@ const Modal: React.FC<ModalProps> = ({
 								<div className='text-lg font-semibold'>
                                     {title}
 								</div>
+							</div>
+							{/*//@ body */}
+							<div
+							className='relative p-6 flex-auto'
+							>
+								{body}
+							</div>
+							{/* footer */}
+							<div className='flex flex-col gap-2 p-6'>
+							<div className='flex flex-row items-center gap-4 w-full'>
+
+							<Button label='reserve'/>
+
+							</div>
 							</div>
                         </div>
                     </div>

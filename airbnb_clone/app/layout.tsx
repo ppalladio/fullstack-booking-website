209e20e305044e3d7df59d1897ec6f42fb01a788
ppalladio <nine.slide@gmail.com>
2023-05-02 12:48:52 +0200

    check session status

--- airbnb_clone/app/layout.tsx
+++ airbnb_clone/app/layout.tsx
@@ -6,6 +6,7 @@ import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from '@/providers/ToasterProvider';
 import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 export const metadata = {
     title: 'Airbnb',
     description: 'Airbnb Clone',
@@ -13,11 +14,12 @@ export const metadata = {
 const font = Montserrat({
     subsets: ['latin'],
 });
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+	const currentUser = await getCurrentUser()
     return (
         <html lang="en">
             <body className={font.className}>
@@ -25,7 +27,7 @@ export default function RootLayout({
 					<ToasterProvider/>
 					<LoginModal/>
                     <RegisterModal />
-                    <Navbar />
+                    <Navbar currentUser={currentUser}/>
                 </ClientOnly>
                 {children}
             </body>

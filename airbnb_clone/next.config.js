c60800f621ff9f7d5181f9fa27acd2395101df98
ppalladio <nine.slide@gmail.com>
2023-05-03 21:46:02 +0200

    add github image to as user image github login is not added as user in mangoDB still!

--- airbnb_clone/next.config.js
+++ airbnb_clone/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: ['avatar.githubusercontent.com'],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

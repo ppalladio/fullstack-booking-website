9e88d962c344f067a507ce909b06db376608a2c5
ppalladio <nine.slide@gmail.com>
2023-05-08 22:50:07 +0200

    reset selected icon by clicking on logo

--- airbnb_clone/app/components/navbar/Logo.tsx
+++ airbnb_clone/app/components/navbar/Logo.tsx
@@ -6,6 +6,7 @@ const Logo = () => {
 	const router = useRouter();
     return (
         <Image
+		onClick={()=> router.push('/')}
             className="hidden md:block cursor-pointer"
             src="/images/logo.png"
             height="100"

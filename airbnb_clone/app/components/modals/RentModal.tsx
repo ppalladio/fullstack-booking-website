e46501d4eb11c2bf3f35374abdcb2a23cd5b6af5
ppalladio <nine.slide@gmail.com>
2023-05-12 22:48:49 +0200

    useCountries customized hooks

--- airbnb_clone/app/components/modals/RentModal.tsx
+++ airbnb_clone/app/components/modals/RentModal.tsx
@@ -7,6 +7,7 @@ import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../inputs/CountrySelect';
 
 enum STEPS {
     CATEGORY = 0,
@@ -88,12 +89,24 @@ const RentModal = () => {
             </div>
         </div>
     );
+
+	if (step === STEPS.LOCATION){
+		bodyContent=(
+			<div className=' flex flex-col gap-8'>
+				<Heading
+				title='where is your place located?'
+				subtitle='help guests find you'
+				/>
+				<CountrySelect />
+			</div>
+		)
+	}
     return (
         <Modal
             isOpen={rentModal.isOpen}
             title="airbnb your home!"
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}

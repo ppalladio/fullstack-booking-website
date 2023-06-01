5558fb8fa0591431b1134d00d229d965d768ea10
ppalladio <nine.slide@gmail.com>
2023-06-02 01:32:50 +0200

    intend to add listing, return 500

---
+++ airbnb_clone/app/api/listings/route.ts
@@ -0,0 +1,40 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import { NextResponse } from 'next/server';
+import prisma from '@/app/libs/prismadb';
+
+export async function POST(request: Request) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+	const body = await request.json();
+	const { 
+	  title,
+	  description,
+	  imageSrc,
+	  category,
+	  roomCount,
+	  bathroomCount,
+	  guestCount,
+	  location,
+	  price,
+	 } = body;
+
+    const listing = await prisma.listing.create({
+        data: {
+			title,
+			description,
+			imageSrc,
+			category,
+			roomCount,
+			bathroomCount,
+			guestsCount: guestCount, //@because in prisma scheme, 	guestsCount Int was used instead of guestCount
+			locationValue: location.value,
+			price: parseInt(price, 10),
+			userId: currentUser.id
+        },
+    });
+
+	return NextResponse.json( listing );
+}
--- airbnb_clone/app/components/modals/RentModal.tsx
+++ airbnb_clone/app/components/modals/RentModal.tsx
@@ -95,7 +95,7 @@ const RentModal = () => {
         setIsLoading(true);
 
         axios
-            .post('./api/listings', data)
+            .post('/api/listings', data)
             .then(() => {
                 toast.success('Listing created successfully');
                 router.refresh();

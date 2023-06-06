839d8c5d212f3e92c83d634332b493bdc32fedee
ppalladio <nine.slide@gmail.com>
2023-06-06 21:34:40 +0200

    filter component && empty state

---
+++ airbnb_clone/app/components/EmptyState.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useRouter } from "next/navigation";
+import Heading from "./Heading";
+import Button from "./Button";
+
+interface EmptyStateProps {
+    title?: string;
+    subtitle?: string;
+    showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+    title= " no exact matches",
+    subtitle = "try change or remove some of your filters",
+    showReset,
+}) => {
+
+	const router = useRouter()
+
+    return <div className="h-[68vh] flex flex-col gap-2 justify-center items-center">
+		<Heading
+		center
+		title={title}
+		subtitle={subtitle}/>
+		<div className="w-48 mt-4">
+{showReset&&(
+	<Button
+	outline
+	label="remove all filters"
+	onClick={() => router.push('/')}
+	/>
+)}
+		</div>
+	</div>;
+};
+
+export default EmptyState;

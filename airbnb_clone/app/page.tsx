01f9c41fe101e1aeed98f4d09e8f63073a2f22d0
ppalladio <nine.slide@gmail.com>
2023-06-07 20:27:46 +0200

    add getListings component call to get listing info

--- airbnb_clone/app/page.tsx
+++ airbnb_clone/app/page.tsx
@@ -1,23 +1,25 @@
+import getListings from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 
-export default function Home() {
+export default async function Home() {
+    const listings = await getListings();
 
-
-	const isEmpty = true;
-	if (isEmpty) {
-		return(
-			<ClientOnly>
-				<EmptyState showReset/>
-			</ClientOnly>
-		)
-	}
+    if (listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState showReset />
+            </ClientOnly>
+        );
+    }
     return (
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                    <div className="capitalize">my future listing </div>
+                    {listings.map((listing: any) => {
+                        return <div>{listing.title}</div>;
+                    })}
                 </div>
             </Container>
         </ClientOnly>

c99cd0172632f985e45afe4e31cc098beb5a57c9
ppalladio <nine.slide@gmail.com>
2023-07-19 19:04:46 +0200

    | + useFavorite hook added for post and delete

---
+++ airbnb_clone/app/hooks/useFavorite.ts
@@ -0,0 +1,57 @@
+import { useRouter } from 'next/navigation';
+import { SafeUser } from '../types';
+import useLoginModal from './useLoginModal';
+import { useCallback, useMemo } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+interface IUseFavoriteProps {
+    listingId: string;
+    currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavoriteProps) => {
+    const route = useRouter();
+    const loginModal = useLoginModal();
+
+    const hasFavorited = useMemo(() => {
+        const list = currentUser?.favoriteIds || [];
+
+        return list.includes(listingId);
+    }, [listingId, currentUser]);
+
+    const toggleFavorite = useCallback(
+        async (e: React.MouseEvent<HTMLDivElement>) => {
+            e.stopPropagation();
+
+            if (!currentUser) {
+                return loginModal.onOpen();
+            }
+
+            try {
+                let request;
+                if (hasFavorited) {
+                    request = () => {
+                        axios.delete('api/favorites/${listingId}');
+                    };
+                } else {
+                    request = () => {
+                        axios.post('api/favorites/${listingId}');
+                    };
+                }
+                await request();
+                route.refresh();
+                toast.success('Added to favorites');
+            } catch (error) {
+                toast.error('Something Went Wrong');
+            }
+        },
+        [currentUser, listingId, loginModal, route, hasFavorited],
+    );
+    return {
+        hasFavorited,
+        toggleFavorite,
+    };
+};
+
+export default useFavorite;

c912246449c7868f2feb50667e44b0d50333efef
ppalladio <nine.slide@gmail.com>
2023-07-19 15:50:34 +0200

    start favorite route and styling add favorite styling by adding the button,

---
+++ airbnb_clone/app/components/HeartButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { SafeUser } from '../types'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+interface HeartButtonProps  {
+	listingId:string,
+	currentUser?: SafeUser | null;
+
+}
+
+const HeartButton:React.FC<HeartButtonProps> = ({listingId, currentUser}) => {
+	const hasFavorited = false
+	const toggleFavorited = () => {
+		
+	}
+	return (
+	<div className='relative hover:opacity-80 transition cursor-pointer' onClick={toggleFavorited}>
+		<AiOutlineHeart 
+		size={28}
+		className='fill-white absolute -top-[2px] -right-[2px]'
+		/>
+		<AiFillHeart
+		size={24}
+		className={hasFavorited?'fill-rose-500':'fill-neutral-500/70'}/>
+	</div>
+  )
+}
+
+export default HeartButton
--- airbnb_clone/app/components/listings/ListingCard.tsx
+++ airbnb_clone/app/components/listings/ListingCard.tsx
@@ -4,8 +4,10 @@ import useCountries from '@/app/hooks/useCountries';
 import { SafeUser } from '@/app/types';
 import { Listing, Reservation } from '@prisma/client';
 import { format } from 'date-fns';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
+import HeartButton from '../HeartButton';
 
 interface ListingCardProps {
     data: Listing;
@@ -26,13 +28,13 @@ const ListingCard: React.FC<ListingCardProps> = ({
     actionId = '',
     currentUser,
 }) => {
-    const router = useRouter;
+    const router = useRouter();
     const { getByValue } = useCountries();
 
     const location = getByValue(data.locationValue);
     const handleCancel = useCallback(
         (e: React.MouseEvent<HTMLButtonElement>) => {
-            e.stopPropagation;
+            e.stopPropagation();
             if (disabled) {
                 return;
             }
@@ -57,10 +59,29 @@ const ListingCard: React.FC<ListingCardProps> = ({
         const start = new Date(reservation.startDate);
         const end = new Date(reservation.endDate);
 
-		return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`;
     }, [reservation]);
 
-    return <div></div>;
+    return (
+        <div
+            className="col-span-1 cursor-pointer group"
+            onClick={() => router.push(`/listings/${data.id}`)}
+        >
+            <div className="flex flex-col gap-2 w-full">
+                <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+                    <Image
+                        fill
+                        src={data.imageSrc}
+                        className="object-cover h-full w-full group-hover:scale-110 transition"
+                        alt="listing"
+                    />
+					<div className='absolute top-3 right-3'>
+<HeartButton listingId={data.id} currentUser={currentUser}/>
+					</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ListingCard;

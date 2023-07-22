61405a6dec2b49250b12e790ab3a809e3cfc26b4
ppalladio <nine.slide@gmail.com>
2023-07-22 17:47:44 +0200

    | + listingInfo comp

--- airbnb_clone/app/listings/[listingId]/ListingClient.tsx
+++ airbnb_clone/app/listings/[listingId]/ListingClient.tsx
@@ -1,5 +1,6 @@
 import Container from '@/app/components/Container';
 import ListingHead from '@/app/components/listings/ListingHead';
+import ListingInfo from '@/app/components/listings/ListingInfo';
 import { categories } from '@/app/components/navbar/Categories';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
@@ -34,6 +35,10 @@ const ListingClient: React.FC<ListingClientProps> = ({
                         id={listing.id}
                         currentUser={currentUser}
                     />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+
+						<ListingInfo/>
+					</div>
                 </div>
             </div>
         </Container>

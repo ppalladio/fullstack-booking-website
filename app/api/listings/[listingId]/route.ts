68c0df758fdc8809c21a601c539947d8c1cfc01e
ppalladio <nine.slide@gmail.com>
2023-08-21 21:31:30 +0200

    delete property route

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -1,7 +1,27 @@
-type Props = {}
-const route = (props: Props) => {
-  return (
-	<div>route</div>
-  )
+import { NextResponse } from 'next/server';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+interface ParamsProps {
+    listingId: string;
 }
-export default route
+export async function DELETE(
+    request: Request,
+    { params }: { params: ParamsProps },
+) {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+    const { listingId } = params;
+    if (!listingId || typeof listingId !== 'string') {
+        throw new Error('Invalid ID');
+    }
+
+    const listing = await prisma?.listing.deleteMany({
+        where: {
+            id: listingId,
+            userId: currentUser.id,
+        },
+    });
+    return NextResponse.json(listing);
+}

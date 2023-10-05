06ae2257c4f7935bcc98f5cb67ad74aea1d3b73c
Yuxuan <nine.slide@gmail.com>
2023-10-05 19:53:28 +0200

    deplot

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	export:"output",
+    export: 'output',
     experimental: {
         appDir: true,
     },
     images: {
-        domains: ['avatar.githubusercontent.com', 'lh3.googleusercontent.com','res.cloudinary.com'],
+        domains: [
+            'avatar.githubusercontent.com',
+            'lh3.googleusercontent.com',
+            'res.cloudinary.com',
+        ],
     },
+    basePath: '/airbnb-clone',
 };
 
 module.exports = nextConfig;
